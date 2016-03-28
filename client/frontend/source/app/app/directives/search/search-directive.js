b25164b7dda85a97d4716605b9c973d42fbb8154
haibui <hoanghaibui2003@gmail.com>
2016-03-28 21:42:01 +0700

    menu-top

---
+++ client/frontend/source/app/app/directives/search/search-directive.js
@@ -0,0 +1,24 @@
+﻿(function () {
+  'use strict';
+
+  angular
+      .module('app')
+      .directive('search', search);
+
+  search.$inject = ['$window'];
+
+  function search($window) {
+    var directive = {
+      link: link,
+      bindToController: true,
+      controller: 'app.searchController as vm',
+      restrict: 'EA',
+      templateUrl: 'app/app/directives/search/_tpl/search.tpl.html'
+    };
+    return directive;
+
+    function link(scope, element, attrs) {
+    }
+  }
+
+})();
