d369c87e45459d53c78d505b6b86ff9fd327e44a
Hai Bui <hoanghaibui2003@gmail.com>
2016-04-08 17:06:05 +0700

    top-icon-menu

---
+++ client/frontend/source/app/app/directives/top-icon-menu/controllers/top-icon-menu-controller.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('app.topIconMenuController', topIconMenuController);
+
+    topIconMenuController.$inject = ['$location'];
+
+    function topIconMenuController($location) {
+        var vm = this;
+        vm.id = 'topIconMenuController';
+        activate();
+
+        function activate() { }
+    }
+})();
---
+++ client/frontend/source/app/app/directives/top-icon-menu/top-icon-menu-directive.js
@@ -0,0 +1,24 @@
+﻿(function () {
+  'use strict';
+
+  angular
+      .module('app')
+      .directive('topIconMenu', topIconMenu);
+
+  topIconMenu.$inject = ['$window'];
+
+  function topIconMenu($window) {
+    var directive = {
+      link: link,
+      bindToController: true,
+      controller: 'app.topIconMenuController as vm',
+      restrict: 'EA',
+      templateUrl: 'app/app/directives/top-icon-menu/_tpl/top-icon-menu.tpl.html'
+    };
+    return directive;
+
+    function link(scope, element, attrs) {
+    }
+  }
+
+})();
