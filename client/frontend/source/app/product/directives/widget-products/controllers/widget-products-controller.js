5e7a63fb83c00f25f424d14238f1b862d6d14e4e
Hai Bui <hoanghaibui2003@gmail.com>
2016-05-13 14:12:44 +0700

    Adjust css

--- client/frontend/source/app/product/directives/widget-products/controllers/widget-products-controller.js
+++ client/frontend/source/app/product/directives/widget-products/controllers/widget-products-controller.js
@@ -11,7 +11,7 @@
         var vm = this;
         vm.id = 'widgetProductsController';
         activate();
-
+      vm.list = [1, 2, 3, 4, 5, 6,8,9,10,11,12,13,14,15];
         function activate() { }
     }
 })();
